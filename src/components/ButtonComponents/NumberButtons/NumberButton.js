57bfd0499c78d637852b0d9ac2d77ff98bef82b7
Vanessa <vanessatellez45@gmail.com>
2019-11-05 21:08:07 -0500

    mvp met

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const NumberButton = () => {
+ const NumberButton = (props) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+      <button>{props.button}</button>}
     </>
   );
-};
+};export default NumberButton;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,21 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { operators } from "../../../data";
+import OperatorButton from "./OperatorButton"
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Operators = () => {
+  const [operatorBtns] = useState(operators)
   // STEP 2 - add the imported data to state
   return (
-    <div>
+    <div className="operatorButtons">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       operatorBtns.map((button, index)=> (
+         <OperatorButton key={index} button={button}/>
+       ))}
     </div>
   );
-};
+}; export default Operators;
